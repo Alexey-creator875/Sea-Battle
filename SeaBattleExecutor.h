f50fbcb931a335e17aa433c0e053165c701d393a
Alexey-creator875 <lexanav1245@mail.ru>
2025-05-27 23:11:24 +0300

    add enum class with menu actions

--- SeaBattleExecutor.h
+++ SeaBattleExecutor.h
@@ -3,6 +3,13 @@
 #include <SFML/Graphics.hpp>
 
 namespace SeaBattleExecutor {
+enum class Action {
+    Wait,
+    StartGame,
+    ShowRules,
+    QuitGame,
+};
+
 void Menu(sf::RenderWindow& window);
 
 void StartGame();
