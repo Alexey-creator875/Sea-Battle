87fde42009a4c13295ef08bb98bb967d4a43b74c
Alexey-creator875 <lexanav1245@mail.ru>
2025-05-27 20:57:56 +0300

    make valid exit button and add center formatting

--- SeaBattleExecutor.cpp
+++ SeaBattleExecutor.cpp
@@ -9,17 +9,15 @@
 #include <stdexcept>
 
 namespace {
-const size_t kWindowLenght = 1200;
-const size_t kWindowHeight = 900;
+const int kDeffaultWindowLenght = 1200;
+const int kDeffaultWindowHeight = 900;
 
-const size_t kOpponentBattleFieldX = 180;
-const size_t kOpponentBattleFieldY = 240;
+const unsigned int kMenuTitulCharacterSize = 100;
+const unsigned int kMenuButtonCharacterSize = 40;
 
-const size_t kPlayerBattleFieldX = 600;
-const size_t kPlayerBattleFieldY = 240;
+const int kMenuTitulCoordinateY = 50;
+const int kMenuButtonCoordinateY = 250;
 
-const size_t kCubePixelSize = 38;
-const size_t kCubeOutlineThikness = 1;
 
 }  // namespace
 
@@ -32,10 +30,11 @@ void Menu(sf::RenderWindow& window) {
     }
 
     // создание прямоугольника куда пихаем текстуру фона
-    sf::RectangleShape backGround(sf::Vector2f(kWindowLenght, kWindowHeight));
+    sf::RectangleShape backGround(sf::Vector2f(kDeffaultWindowLenght, kDeffaultWindowHeight));
     backGround.setTexture(&menuBackground);
-    backGround.setPosition(sf::Vector2f(0, 0));
     window.draw(backGround);
+
+
 
     // загружаем шрифт
     sf::Font helveticaFont;
@@ -45,56 +44,61 @@ void Menu(sf::RenderWindow& window) {
 
     // создаём лейбл для названия игры
     sf::Text titul(helveticaFont, sf::String("Sea Battle"));
-    titul.setPosition(sf::Vector2f(400, 50));
-    titul.setFillColor(sf::Color(172, 232, 228));
-    titul.setCharacterSize(100);
+    titul.setFillColor(sf::Color::Blue);
+    titul.setCharacterSize(kMenuTitulCharacterSize);
     titul.setOutlineThickness(3);
     titul.setOutlineColor(sf::Color::White);
+
+    float titulCoordinateX = (kDeffaultWindowLenght / 2) - titul.getGlobalBounds().getCenter().x;
+    titul.setPosition(sf::Vector2f(titulCoordinateX, kMenuTitulCoordinateY));
+
     window.draw(titul);
 
+    // создаём надпись кнопки, котрую наложим поверх кнопки
+    sf::Text buttonText(helveticaFont, sf::String("Exit the game"));
+    buttonText.setFillColor(sf::Color::Blue);
+    buttonText.setCharacterSize(kMenuButtonCharacterSize);
+
+    float buttonCoordinateX = (kDeffaultWindowLenght / 2) - buttonText.getGlobalBounds().getCenter().x;
+    buttonText.setPosition(sf::Vector2f(buttonCoordinateX, kMenuButtonCoordinateY));
+
     // создаём кнопку на базе RectangeShape
-    sf::RectangleShape button({200.f,150.f});
-    button.setPosition(sf::Vector2f(600,300));
+    sf::RectangleShape button;
+    button.setPosition(buttonText.getPosition());
+    float buttonSizeX = (buttonText.getGlobalBounds().getCenter().x - buttonText.getPosition().x) * 2;
+    float buttonSizeY = (buttonText.getGlobalBounds().getCenter().y - buttonText.getPosition().y) * 2;
+
+    button.setSize(sf::Vector2f(buttonSizeX, buttonSizeY));
     button.setFillColor(sf::Color(0, 0, 0, 0));
+
+    window.draw(buttonText);
     window.draw(button);
 
-    // создаём надпись кнопки, котрую наложим поверх кнопки
-    sf::Text buttonText(helveticaFont, sf::String("Push me"));
-    buttonText.setPosition(button.getPosition());
-    buttonText.setFillColor(sf::Color::Blue);
-    buttonText.setCharacterSize(60);
-    window.draw(buttonText);
+    bool menuContinueExecution = true;
+    int action = 0;
 
-
-    // std::cout << button.getGlobalBounds().getCenter().x << ' ' << button.getGlobalBounds().getCenter().x << '\n';
-
-    bool continueExecution = true;
-    int menuNum = 0;
-
-    while (continueExecution) {
+    while (menuContinueExecution) {
+        while (const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()) {
+                window.close();
+                return;
+            }
+        }
 
         buttonText.setFillColor(sf::Color::Blue);
 
-        menuNum = 0;
-        // auto mousePos = static_cast<sf::Vector2f>(sf::Mouse::getPosition());
-        
-        auto mousePosRelatedOnWindow = static_cast<sf::Vector2f>(sf::Mouse::getPosition() - window.getPosition());
+        action = 0;
 
-        std::cout << mousePosRelatedOnWindow.x << ' ' << mousePosRelatedOnWindow.y << '\n';
-        std::cout << button.getGlobalBounds().position.x << ' ' << button.getGlobalBounds().position.y << '\n';
-        // auto checkingRectangle = buttonText.getGlobalBounds();
-
-        // std::cout << button.getGlobalBounds().getCenter().x << ' ' << button.getGlobalBounds().getCenter().x << '\n';
-
-        if (button.getGlobalBounds().contains(mousePosRelatedOnWindow)) {
+        if (button.getGlobalBounds().contains(static_cast<sf::Vector2f>(sf::Mouse::getPosition(window)))) {
             buttonText.setFillColor(sf::Color::Red);
-            menuNum = 1;
+            action = 1;
         }
 
         if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {
-            if (menuNum == 1) {
-                std::cout << "it works!\n";
-                continueExecution = false;
+            if (action == 1) {
+                std::cout << "it works! Exit.\n";
+                window.close();
+                return;
             }
         }
 
@@ -110,22 +114,7 @@ void Menu(sf::RenderWindow& window) {
 }
 
 void RunApplication() {
-    sf::RenderWindow window(sf::VideoMode({kWindowLenght, kWindowHeight}), "Sea Battle");
-
-    // sf::Texture empty;
-    // if (!empty.loadFromFile("../images/dot.jpg")) {
-    //     throw std::runtime_error("failed to open file");
-    // }
-
-    // sf::Texture unknown;
-    // if (!unknown.loadFromFile("../images/unknown.jpg")) {
-    //     throw std::runtime_error("failed to open file");
-    // }
-
-    // sf::Texture hitted;
-    // if (!hitted.loadFromFile("../images/cross.webp.png")) {
-    //     throw std::runtime_error("failed to open file");
-    // }
+    sf::RenderWindow window(sf::VideoMode({kDeffaultWindowLenght, kDeffaultWindowHeight}), "Sea Battle");
     
     while (window.isOpen()) {
         while (const std::optional event = window.pollEvent()) {
@@ -133,50 +122,9 @@ void RunApplication() {
                 window.close();
         }
 
-        window.clear(sf::Color(186, 206, 219));
+        // window.clear(sf::Color(186, 206, 219));
 
         Menu(window);
-
-
-        // int size = 10;
-
-        // for (int i = 0; i < size; ++i) {
-        //     for (int j = 0; j < size; ++j) {
-        //         sf::RectangleShape cube(sf::Vector2f(kCubePixelSize, kCubePixelSize));
-        //         cube.setFillColor(sf::Color(136, 204, 252));
-        //         cube.setPosition(sf::Vector2f(kOpponentBattleFieldX + j * (kCubePixelSize + 2 * kCubeOutlineThikness), kOpponentBattleFieldY + i * (kCubePixelSize + 2 * kCubeOutlineThikness)));
-        //         cube.setOutlineThickness(kCubeOutlineThikness);
-        //         cube.setOutlineColor(sf::Color::White);
-                
-        //         cube.setTexture(&empty);
-
-        //         window.draw(cube);
-        //     }
-        // }
-
-        // sf::RectangleShape cube(sf::Vector2f(kCubePixelSize, kCubePixelSize));
-        // cube.setFillColor(sf::Color(136, 204, 252));
-        // cube.setPosition(sf::Vector2f(kOpponentBattleFieldX, kOpponentBattleFieldY));
-        // cube.setOutlineThickness(kCubeOutlineThikness);
-        // cube.setOutlineColor(sf::Color::White);
-        
-        // cube.setTexture(&hitted);
-
-        // window.draw(cube);
-
-        // for (int i = 0; i < size; ++i) {
-        //     for (int j = 0; j < size; ++j) {
-        //         sf::RectangleShape cube(sf::Vector2f(kCubePixelSize, kCubePixelSize));
-        //         cube.setFillColor(sf::Color(136, 204, 252));
-        //         cube.setPosition(sf::Vector2f(kPlayerBattleFieldX + j * (kCubePixelSize + 2 * kCubeOutlineThikness), kPlayerBattleFieldY + i * (kCubePixelSize + 2 * kCubeOutlineThikness)));
-        //         cube.setOutlineThickness(kCubeOutlineThikness);
-        //         cube.setOutlineColor(sf::Color::White);
-                
-        //         cube.setTexture(&empty);
-
-        //         window.draw(cube);
-        //     }
-        // }
 
         window.display();
     }
