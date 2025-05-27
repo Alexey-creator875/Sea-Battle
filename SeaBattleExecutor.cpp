ec19384c106d8785e754a2249a4eae0d2a424b9e
Alexey-creator875 <lexanav1245@mail.ru>
2025-05-27 23:10:42 +0300

    add start game, show rules and quit game buttons

--- SeaBattleExecutor.cpp
+++ SeaBattleExecutor.cpp
@@ -13,10 +13,14 @@ const int kDeffaultWindowLenght = 1200;
 const int kDeffaultWindowHeight = 900;
 
 const unsigned int kMenuTitulCharacterSize = 100;
-const unsigned int kMenuButtonCharacterSize = 40;
+const unsigned int kMenuButtonsCharacterSize = 40;
 
 const int kMenuTitulCoordinateY = 50;
-const int kMenuButtonCoordinateY = 250;
+const int kMenuStartGameButtonCoordinateY = 250;
+const int kMenuShowRulesButtonCoordinateY = 350;
+const int kMenuQuitGameButtonCoordinateY = 450;
+
+
 
 
 }  // namespace
@@ -54,28 +58,72 @@ void Menu(sf::RenderWindow& window) {
 
     window.draw(titul);
 
-    // создаём надпись кнопки, котрую наложим поверх кнопки
-    sf::Text buttonText(helveticaFont, sf::String("Exit the game"));
-    buttonText.setFillColor(sf::Color::Blue);
-    buttonText.setCharacterSize(kMenuButtonCharacterSize);
 
-    float buttonCoordinateX = (kDeffaultWindowLenght / 2) - buttonText.getGlobalBounds().getCenter().x;
-    buttonText.setPosition(sf::Vector2f(buttonCoordinateX, kMenuButtonCoordinateY));
+    // создаём надпись кнопки, запускающей игру
+    sf::Text startGameButtonText(helveticaFont, sf::String("Start the game"));
+    startGameButtonText.setFillColor(sf::Color::Blue);
+    startGameButtonText.setCharacterSize(kMenuButtonsCharacterSize);
 
-    // создаём кнопку на базе RectangeShape
-    sf::RectangleShape button;
-    button.setPosition(buttonText.getPosition());
-    float buttonSizeX = (buttonText.getGlobalBounds().getCenter().x - buttonText.getPosition().x) * 2;
-    float buttonSizeY = (buttonText.getGlobalBounds().getCenter().y - buttonText.getPosition().y) * 2;
+    float startGameButtonCoordinateX = (kDeffaultWindowLenght / 2) - startGameButtonText.getGlobalBounds().getCenter().x;
+    startGameButtonText.setPosition(sf::Vector2f(startGameButtonCoordinateX, kMenuStartGameButtonCoordinateY));
 
-    button.setSize(sf::Vector2f(buttonSizeX, buttonSizeY));
-    button.setFillColor(sf::Color(0, 0, 0, 0));
+    // создаём кнопку, запускающую игру
+    sf::RectangleShape startGameButton;
+    startGameButton.setPosition(startGameButtonText.getPosition());
+    float startGameButtonSizeX = (startGameButtonText.getGlobalBounds().getCenter().x - startGameButtonText.getPosition().x) * 2;
+    float startGameButtonSizeY = (startGameButtonText.getGlobalBounds().getCenter().y - startGameButtonText.getPosition().y) * 2;
 
-    window.draw(buttonText);
-    window.draw(button);
+    startGameButton.setSize(sf::Vector2f(startGameButtonSizeX, startGameButtonSizeY));
+    startGameButton.setFillColor(sf::Color(0, 0, 0, 0));
+
+
+    // создаём надпись кнопки, показывающей правила игры
+    sf::Text showRulesButtonText(helveticaFont, sf::String("Check Rules"));
+    showRulesButtonText.setFillColor(sf::Color::Blue);
+    showRulesButtonText.setCharacterSize(kMenuButtonsCharacterSize);
+
+    float showRulesButtonCoordinateX = (kDeffaultWindowLenght / 2) - showRulesButtonText.getGlobalBounds().getCenter().x;
+    showRulesButtonText.setPosition(sf::Vector2f(showRulesButtonCoordinateX, kMenuShowRulesButtonCoordinateY));
+
+    // создаём кнопку, показывающую правила игры
+    sf::RectangleShape showRulesButton;
+    showRulesButton.setPosition(showRulesButtonText.getPosition());
+    float showRulesButtonSizeX = (showRulesButtonText.getGlobalBounds().getCenter().x - showRulesButtonText.getPosition().x) * 2;
+    float showRulesButtonSizeY = (showRulesButtonText.getGlobalBounds().getCenter().y - showRulesButtonText.getPosition().y) * 2;
+
+    showRulesButton.setSize(sf::Vector2f(showRulesButtonSizeX, showRulesButtonSizeY));
+    showRulesButton.setFillColor(sf::Color(0, 0, 0, 0));
+    
+
+    // создаём надпись кнопки, закрывающей игру
+    sf::Text quitGameButtonText(helveticaFont, sf::String("Exit the game"));
+    quitGameButtonText.setFillColor(sf::Color::Blue);
+    quitGameButtonText.setCharacterSize(kMenuButtonsCharacterSize);
+
+    float quitGameButtonCoordinateX = (kDeffaultWindowLenght / 2) - quitGameButtonText.getGlobalBounds().getCenter().x;
+    quitGameButtonText.setPosition(sf::Vector2f(quitGameButtonCoordinateX, kMenuQuitGameButtonCoordinateY));
+
+    // создаём кнопку, закрывающую игру
+    sf::RectangleShape quitGameButton;
+    quitGameButton.setPosition(quitGameButtonText.getPosition());
+    float quitGameButtonSizeX = (quitGameButtonText.getGlobalBounds().getCenter().x - quitGameButtonText.getPosition().x) * 2;
+    float quitGameButtonSizeY = (quitGameButtonText.getGlobalBounds().getCenter().y - quitGameButtonText.getPosition().y) * 2;
+
+    quitGameButton.setSize(sf::Vector2f(quitGameButtonSizeX, quitGameButtonSizeY));
+    quitGameButton.setFillColor(sf::Color(0, 0, 0, 0));
+
+
+    window.draw(startGameButtonText);
+    window.draw(startGameButton);
+
+    window.draw(showRulesButtonText);
+    window.draw(showRulesButton);
+
+    window.draw(quitGameButtonText);
+    window.draw(quitGameButton);
 
     bool menuContinueExecution = true;
-    int action = 0;
+    Action menuAction = Action::Wait;
 
     while (menuContinueExecution) {
         while (const std::optional event = window.pollEvent()) {
@@ -85,27 +133,55 @@ void Menu(sf::RenderWindow& window) {
             }
         }
 
-        buttonText.setFillColor(sf::Color::Blue);
+        startGameButtonText.setFillColor(sf::Color::Blue);
+        showRulesButtonText.setFillColor(sf::Color::Blue);
+        quitGameButtonText.setFillColor(sf::Color::Blue);
 
-        action = 0;
+        menuAction = Action::Wait;
 
-        if (button.getGlobalBounds().contains(static_cast<sf::Vector2f>(sf::Mouse::getPosition(window)))) {
-            buttonText.setFillColor(sf::Color::Red);
-            action = 1;
+        if (startGameButton.getGlobalBounds().contains(static_cast<sf::Vector2f>(sf::Mouse::getPosition(window)))) {
+            startGameButtonText.setFillColor(sf::Color::Red);
+            menuAction = Action::StartGame;
+        }
+
+        if (showRulesButton.getGlobalBounds().contains(static_cast<sf::Vector2f>(sf::Mouse::getPosition(window)))) {
+            showRulesButtonText.setFillColor(sf::Color::Red);
+            menuAction = Action::ShowRules;
+        }
+
+        if (quitGameButton.getGlobalBounds().contains(static_cast<sf::Vector2f>(sf::Mouse::getPosition(window)))) {
+            quitGameButtonText.setFillColor(sf::Color::Red);
+            menuAction = Action::QuitGame;
         }
 
         if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {
-            if (action == 1) {
-                std::cout << "it works! Exit.\n";
-                window.close();
-                return;
+            switch (menuAction) {
+                case Action::Wait:
+                    break;
+                case Action::StartGame:
+                    std::cout << "Start Game\n";
+                    break;
+                case Action::ShowRules:
+                    std::cout << "Show Rules\n";
+                    break;
+                case Action::QuitGame:
+                    std::cout << "Show Rules\n";
+                    window.close();
+                    return;
             }
         }
 
         window.draw(backGround);
         window.draw(titul);
-        window.draw(button);
-        window.draw(buttonText);
+
+        window.draw(startGameButton);
+        window.draw(startGameButtonText);
+
+        window.draw(showRulesButton);
+        window.draw(showRulesButtonText);
+
+        window.draw(quitGameButton);
+        window.draw(quitGameButtonText);
 
         window.display();
     }
